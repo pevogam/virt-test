618d20850627f987aec798eea5a543eb952af395
Plamen Dimitrov <pdimitrov@pevogam.com>
2018-05-28 01:14:38 +0700

    avoid circular import

--- virttest/staging/__init__.py
+++ virttest/staging/__init__.py
@@ -1,6 +1,7 @@
 __all__ = ['service', 'utils_cgroup', 'utils_koji', 'utils_memory']
-import service
-import utils_cgroup
-import utils_koji
-import utils_memory
-import backports
+# TODO: circular import?
+#from . import service
+from . import utils_cgroup
+from . import utils_koji
+from . import utils_memory
+from . import backports
